2772389e8143b6e1e3b03b440d8acca5b99ca22c
PrajwalBhattarai123 <np05cp4s230051@iic.edu.np>
2025-01-17 11:14:37 +0545

    code complete with all features

---
+++ Models/CurrencyTypeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayTrack.Models
+{
+    public class CurrencyTypeModel
+    {
+        //USD NRS INR GBP
+        public string CurrencyType { get; set; }
+
+    }
+}
